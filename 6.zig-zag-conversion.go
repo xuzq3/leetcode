b134072935eb69fbfc7d86df960e97a38dcedf89
xzq <xuziqiang@singworld.net>
2020-04-15 11:12:06 +0800

    add 6

---
+++ 6.zig-zag-conversion.go
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=6 lang=golang
+ *
+ * [6] ZigZag Conversion
+ *
+ * https://leetcode.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (35.13%)
+ * Likes:    1493
+ * Dislikes: 4264
+ * Total Accepted:    428.7K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+ * of rows like this: (you may want to display this pattern in a fixed font for
+ * better legibility)
+ * 
+ * 
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 
+ * 
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * 
+ * Write the code that will take a string and make this conversion given a
+ * number of rows:
+ * 
+ * 
+ * string convert(string s, int numRows);
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * 
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * 
+ */
+
+// @lc code=start
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+
+	n := len(s)
+	buf := new(bytes.Buffer)
+	round := numRows*2 - 2
+
+	for i := 0; i < numRows; i++ {
+		p := i
+		for p < n {
+			buf.WriteByte(s[p])
+
+			if i != 0 && i != numRows-1 {
+				t := p + round - i*2
+				if t < n {
+					buf.WriteByte(s[t])
+				}
+			}
+			p += round
+		}
+	}
+	return buf.String()
+}
+// @lc code=end
+
