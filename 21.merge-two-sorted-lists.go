73a0126aba7e14673eb8fcb5b8127bf4d3e85f3d
xzq <xuziqiang@singworld.net>
2020-05-15 09:12:46 +0800

    add 21

--- 21.merge-two-sorted-lists.go
+++ 21.merge-two-sorted-lists.go
@@ -32,8 +32,40 @@
  *     Next *ListNode
  * }
  */
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-    
+ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+
+	head := &ListNode{}
+	cur := head
+	for l1 != nil || l2 != nil {
+		if l1 != nil && l2 != nil {
+			if l1.Val < l2.Val {
+				cur.Next = &ListNode{
+					Val: l1.Val,
+				}
+				l1 = l1.Next
+			} else {
+				cur.Next = &ListNode{
+					Val: l2.Val,
+				}
+				l2 = l2.Next
+			}
+		} else if l1 != nil {
+			cur.Next = &ListNode{
+				Val: l1.Val,
+			}
+			l1 = l1.Next
+		} else {
+			cur.Next = &ListNode{
+				Val: l2.Val,
+			}
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+	return head.Next
 }
 // @lc code=end
 
