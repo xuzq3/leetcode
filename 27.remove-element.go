91f1b0df6c85600d33d6e8ec9ab1272cce8cc0d9
xzq <xuziqiang@singworld.net>
2020-06-03 16:31:48 +0800

    add 27

--- 27.remove-element.go
+++ 27.remove-element.go
@@ -1,44 +1,49 @@
 /*
- * @lc app=leetcode id=26 lang=golang
+ * @lc app=leetcode id=27 lang=golang
  *
- * [26] Remove Duplicates from Sorted Array
+ * [27] Remove Element
  *
- * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ * https://leetcode.com/problems/remove-element/description/
  *
  * algorithms
- * Easy (43.84%)
- * Likes:    2416
- * Dislikes: 4954
- * Total Accepted:    943.9K
- * Total Submissions: 2.1M
- * Testcase Example:  '[1,1,2]'
+ * Easy (47.30%)
+ * Likes:    1363
+ * Dislikes: 2597
+ * Total Accepted:    594.3K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,2,2,3]\n3'
  *
- * Given a sorted array nums, remove the duplicates in-place such that each
- * element appear only once and return the new length.
+ * Given an array nums and a value val, remove all instances of that value
+ * in-place and return the new length.
  * 
  * Do not allocate extra space for another array, you must do this by modifying
  * the input array in-place with O(1) extra memory.
  * 
+ * The order of elements can be changed. It doesn't matter what you leave
+ * beyond the new length.
+ * 
  * Example 1:
  * 
  * 
- * Given nums = [1,1,2],
+ * Given nums = [3,2,2,3], val = 3,
  * 
  * Your function should return length = 2, with the first two elements of nums
- * being 1 and 2 respectively.
+ * being 2.
  * 
  * It doesn't matter what you leave beyond the returned length.
+ * 
  * 
  * Example 2:
  * 
  * 
- * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ * Given nums = [0,1,2,2,3,0,4,2], val = 2,
  * 
  * Your function should return length = 5, with the first five elements of nums
- * being modified to 0, 1, 2, 3, and 4 respectively.
+ * containing 0, 1, 3, 0, and 4.
+ * 
+ * Note that the order of those five elements can be arbitrary.
  * 
  * It doesn't matter what values are set beyond the returned length.
- * 
  * 
  * Clarification:
  * 
@@ -51,7 +56,7 @@
  * 
  * 
  * // nums is passed in by reference. (i.e., without making a copy)
- * int len = removeDuplicates(nums);
+ * int len = removeElement(nums, val);
  * 
  * // any modification to nums in your function would be known by the caller.
  * // using the length returned by your function, it prints the first len
@@ -62,20 +67,16 @@
  */
 
 // @lc code=start
-func removeDuplicates(nums []int) int {
-	if len(nums) <= 1 {
-		return len(nums)
-	}
+func removeElement(nums []int, val int) int {
 	j := 0
-	for i := 1; i < len(nums); i++ {
-		if nums[i] != nums[j] {
-			j++
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
 			nums[j] = nums[i]
+			j++
 		}
 	}
-	n := j + 1
-	nums = nums[0:n]
-	return n
+	nums = nums[0:j]
+	return j
 }
 // @lc code=end
 
